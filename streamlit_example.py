c3156d0348e8061a0d4b6043e15ef977dc7ce307
Bruce Nielson <brucenielson1@gmail.com>
2025-12-04 12:14:21 -0700

    Update requirements

--- streamlit_example.py
+++ streamlit_example.py
@@ -2,10 +2,12 @@ import streamlit as st
 import ollama
 import time
 
+
 def stream_data(text, delay: float=0.02):
     for word in text.split():
         yield word + " "
         time.sleep(delay)
+
 
 # Input for the prompt
 prompt = st.chat_input("Ask DeepSeek R1")
